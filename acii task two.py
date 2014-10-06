20cf31b305ff79c0e85b88dbcd21678b088fede2
ashleynkomo <ankomo@longroad.ac.uk>
2014-10-06 21:10:31 +0100

    ascii task one

---
+++ acii task two.py
@@ -0,0 +1,9 @@
+#Ashley Nkomo
+#03-10-14
+#Ascii Task One-2
+
+character = input("please enter a character: ")
+
+ascii_equivalent = ord(character)
+
+print("The ascii equivalent is {0}".format(ascii_equivalent))
---
+++ ascii task one.py
@@ -0,0 +1,9 @@
+#Ashley Nkomo
+#03-10-14
+#Ascii Task One-1
+
+code = int(input("please enter an Ascii code: "))
+
+character = chr(code)
+
+print("The text character of the ascii code entered is {0}".format(character))
