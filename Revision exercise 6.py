b1790109c6c41af16ff551c0de05c75b15849a06
ashleynkomo <ankomo@longroad.ac.uk>
2014-10-06 20:28:59 +0100

    Selection work

--- Revision exercise 6.py
+++ Revision exercise 6.py
@@ -7,11 +7,11 @@ number2 = float(input("Please enter a second number: "))
 number3 = float(input("Please enter a third number: "))
 
 if number3 > number1 and number2:
-    print("The  number entered is larger")
+    print("The  third number entered is larger")
 elif number2 > number1 and number3 :
     print("The second number entered is larger")
 elif number1 > number2 and number3:
-    print("The third number entered is larger")
+    print("The first number entered is larger")
     
 else:
     print("The numbers are exactly the same")
