70d4bd79d3573e11c6f5d358c26eca8dc2dadb53
Ashley Nkomo <41430@longroad.ac.uk>
2014-10-07 11:10:09 +0100

    dev ex 4

---
+++ development exercise 4.py
@@ -0,0 +1,18 @@
+#Ashley Nkomo
+#07-10-14
+#Development Exercise 4
+
+mark = input("Please enter your mark: ")
+
+if 81 < mark > 100:
+    print("A grade")
+elif 71 <mark > 80 :
+    print("B grade")
+elif 61 < mark > 70:
+    print("C grade")
+elif mark > 60:
+    print("D grade")
+elif mark > 50:
+    print("E grade")
+elif mark > 50:
+    print("E grade")
